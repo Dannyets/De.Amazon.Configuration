355acf2eac129ecfe46e8a9508a85c6149929ebf
Danny Etsebban <dannyets@gmail.com>
2019-06-11 01:25:50 +0300

    Added basic implementation

---
+++ De.Amazon.Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using De.Amazon.Configuration.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace De.Amazon.Configuration.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddAwsConfiguration(this IServiceCollection services)
+        {
+            var accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY");
+            var secretKey = Environment.GetEnvironmentVariable("AWS_SECRET_KEY");
+
+            var region = RegionEndpoint.USEast2;
+
+            var credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+            var amazonConfig = new AmazonConfiguration(credentials, region);
+
+            services.AddSingleton(amazonConfig);
+        }
+    }
+}
---
+++ De.Amazon.Configuration/Models/AmazonConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Amazon;
+using Amazon.Runtime;
+
+namespace De.Amazon.Configuration.Models
+{
+    public class AmazonConfiguration
+    {
+        public AmazonConfiguration(AWSCredentials credentials,
+                                   RegionEndpoint regionEndpoint)
+        {
+            Credentials = credentials;
+            RegionEndpoint = regionEndpoint;
+        }
+
+        public AWSCredentials Credentials { get; }
+
+        public RegionEndpoint RegionEndpoint { get; }
+    }
+}
